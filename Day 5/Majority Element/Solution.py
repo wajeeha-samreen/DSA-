b0b7b28901172461485488a52697cc6ac23f1142
WAJEEHA SAMREEN <145857394+wajeeha-samreen@users.noreply.github.com>
2024-10-28 23:27:07 +0530

    Create Solution.py

---
+++ Day 5/Majority Element/Solution.py
@@ -0,0 +1,10 @@
+#Using Moore’s Voting Algorithm
+class Solution(object):
+    def majorityElement(self, nums):
+        sol = None
+        cnt = 0
+        for i in nums:
+            if cnt == 0:
+                sol = i
+            cnt += (1 if i == sol else -1)
+        return sol
