4ccce162254d3463bb284a78ea4517b2a951a678
WAJEEHA SAMREEN <145857394+wajeeha-samreen@users.noreply.github.com>
2024-11-02 14:38:04 +0530

    Create Solution.py

---
+++ Day 9/Rabin-Karp Algorithm for Pattern Searching/Solution.py
@@ -0,0 +1,34 @@
+d = 256
+
+def search(pat, txt, q):
+    M = len(pat)
+    N = len(txt)
+    p = 0
+    t = 0
+    h = 1
+
+    for i in range(M-1):
+        h = (h*d) % q
+
+    for i in range(M):
+        p = (d*p + ord(pat[i])) % q
+        t = (d*t + ord(txt[i])) % q
+
+    for i in range(N-M+1):
+        if p == t:
+            for j in range(M):
+                if txt[i+j] != pat[j]:
+                    break
+            if j == M-1:
+                print("Pattern found at index " + str(i))
+
+        if i < N-M:
+            t = (d*(t-ord(txt[i])*h) + ord(txt[i+M])) % q
+            if t < 0:
+                t = t+q
+
+if __name__ == '__main__':
+    txt = "THIS IS A TEST TEXT"
+    pat = "Test"
+    q = 101
+    search(pat, txt, q)
