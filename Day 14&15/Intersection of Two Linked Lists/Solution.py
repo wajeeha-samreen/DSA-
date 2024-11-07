b0243bafd1dbd919981775938708900d843c07eb
WAJEEHA SAMREEN <145857394+wajeeha-samreen@users.noreply.github.com>
2024-11-07 23:51:54 +0530

    Create Solution.py

---
+++ Day 14&15/Intersection of Two Linked Lists/Solution.py
@@ -0,0 +1,9 @@
+class Solution:
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
+        if headA and headB:
+            A, B = headA, headB
+            while A != B:
+                A = A.next if A else headB
+                B = B.next if B else headA
+            return B
+        
